5529bb4a3beb9225e4277af1bea611c01bf258a6
nam-ayeon <boalove284@naver.com>
2023-01-28 16:32:11 +0900

    코드 단축

--- leetCode/src/leetCode/twoSum.java
+++ leetCode/src/leetCode/twoSum.java
@@ -1,11 +1,11 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class twoSum {
-	
-	
+
 	public static int[] twoSum(int[] nums, int target) {
 
 		// [2,7,6,9] target = 9
@@ -28,22 +28,30 @@ public class twoSum {
 			}
 		}
 		return new int[2];
-		
+
 	}
-		
 
 	public static void main(String[] args) {
-		
-			int[] arr = new int[]{3,4,20,1, 9,0};
 
-		
-			int[] newArr = twoSum(arr, 21);
-		
-		
-			
+		int[] arr = new int[] { 3, 4, 4, 1, 9, 0 };
+
+		int[] newArr = twoSum(arr, 8);
+
+		int[] Arr = twoSumUsingHashMap.twoSumUsingHashMap(arr, 8);
+
+		for (int i = 0; i < newArr.length; i++) {
+
+			System.out.println(newArr[i]);
+
+		}
+		System.out.println("==============");
+
+		for (int i = 0; i < Arr.length; i++) {
+
+			System.out.println(Arr[i]);
+
+		}
+
 	}
-	
-		
-		
-	
+
 }
--- leetCode/src/leetCode/twoSumUsingHashMap.java
+++ leetCode/src/leetCode/twoSumUsingHashMap.java
@@ -4,32 +4,24 @@ import java.util.HashMap;
 
 public class twoSumUsingHashMap {
 
-	
-	
-public static int[] twoSumUsingHashMap(int[] nums, int target) {
-		
-		
+	public static int[] twoSumUsingHashMap(int[] nums, int target) {
+
 		HashMap<Integer, Integer> restOfTarget = new HashMap<Integer, Integer>();
-		
-		int numSize = nums.length;
-		
-		for(int i=0; i< numSize; i++) {
-			
-			int temp = target - nums[i];
-			
-			if(restOfTarget.containsKey(temp)) {
-				return new int[] { restOfTarget.get(temp), i };
-				
-			}else  {
-				restOfTarget.put( target-temp, i );
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (restOfTarget.containsKey(target - nums[i])) {
+
+				return new int[] { restOfTarget.get(target - nums[i]), i };
+
+			} else {
+				restOfTarget.put(nums[i], i);
 			}
-			
+
 		}
-		
-		
-		
+
 		return new int[2];
-				
+
 	}
-		
+
 }
