9a78abab7dc073c2d8a656940bf80f5132dcb09c
아연 <89577407+AyeonnNam@users.noreply.github.com>
2023-02-04 15:17:44 +0900

    Time: 5 ms (25.96%), Space: 42.3 MB (44.76%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -8,13 +8,15 @@ class Solution {
         //배열 정렬 
         Arrays.sort(arr1);
         
+        //맵에 arr1의 값과 횟수를 저장한다.
         for(int i=0; i< arr1.length; i++) {
             map.put(arr1[i], map.getOrDefault(arr1[i],0) + 1);    
         }
         
+        //리스트 선언
         List<Integer> list = new ArrayList<Integer>();
 
-        
+        //arr에 있는 값이 맵에 있으면 해당 값의 횟수만큼 리스트에 넣기
         for(int i=0; i<arr2.length; i++){
             if(map.containsKey(arr2[i])){
                 for(int j=0; j<map.get(arr2[i]); j++) {
@@ -23,7 +25,8 @@ class Solution {
             }
         }
         
-        for(int i=0; i< arr1.length; i++){
+        //남아있는 arr1값 list에 넣기 
+        for(int i=0; i< n; i++){
             if(map.containsKey(arr1[i])&&map.get(arr1[i]) > 0) {
                 for(int j = 0; j< map.get(arr1[i]) ; j ++) {
                     list.add(arr1[i]);
